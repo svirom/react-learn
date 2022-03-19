343ff289aa9275a4dad701e9e38b0d536db7b279
svirom <svirom@yahoo.com>
2022-03-19 20:11:30 +0200

    useState form

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 // function ExpenseItem(props) {
-
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
 
   const clickHandler = () => {
-    title = 'Updated!';
+    setTitle('Updated!');
+    console.log(title);
   }
 
   return (
