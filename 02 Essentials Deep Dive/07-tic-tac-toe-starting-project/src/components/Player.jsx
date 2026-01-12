abadda3b4ff9f9ae59a654a7ae6ddbfbd70fcdda
svirom <svirom@yahoo.com>
2026-01-13 01:48:44 +0200

    essentials deep dive

--- 02 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 02 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,29 +1,40 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing); // best practice updating state based on old state
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
     setPlayerName(event.target.value);
   }
 
-  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let editablePlayerName = <span className='player-name'>{playerName}</span>;
 
   if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+    editablePlayerName = (
+      <input type='text' required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
     <li className={isActive ? 'active' : undefined}>
-      <span className="player">
+      <span className='player'>
         {/*isEditing ? <input type="text" required /> : <span className="player-name">{name}</span>*/}
         {editablePlayerName}
-        <span className="player-symbol">{symbol}</span>
+        <span className='player-symbol'>{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
