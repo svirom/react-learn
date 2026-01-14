6487988cf0c0f8485d6eb1705759832769e0d3e5
svirom <svirom@yahoo.com>
2026-01-14 23:58:54 +0200

    starting investment calculator

---
+++ 03 Essentials Practice/01-investment-calculator/src/components/UserInput.jsx
@@ -0,0 +1,26 @@
+export default function UserInput() {
+  return (
+    <section id='user-input'>
+      <div id='input-group'>
+        <p>
+          <label>Initial Investment</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input type="number" required />
+        </p>
+      </div>
+      <div id='input-group'>
+        <p>
+          <label>Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input type="number" required />
+        </p>
+      </div>
+    </section>
+  );
+}
